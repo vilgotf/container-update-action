0189adfdbbc2d61f53bb7b6281f53b175b9af018
Vilgot Fredenberg <vilgot@fredenberg.xyz>
2020-12-28 22:59:47 +0100

    Clean up code

--- script.sh
+++ script.sh
@@ -16,11 +16,10 @@ source data
 skopeo inspect docker://docker.io/$baseimage | jq -r .Created > baseimage_date &
 skopeo inspect docker://docker.io/$image | jq -r .Created > image_date &
 
-[[ -n $pypi ]] && USE_PYPI=true || USE_PYPI=false
+[[ -n $pypi_project ]] && pypi=true || pypi=false
 
-if [[ $USE_PYPI == true ]]; then
-	INPUT_PYPI=${pypi,,} # make lowercase
-	pypi_data="$(curl -fsSL https://pypi.org/pypi/$INPUT_PYPI/json)"
+if [[ $pypi == true ]]; then
+	pypi_data=$(curl -fsSL https://pypi.org/pypi/$pypi_project/json)
 	readonly pypi_date=$(jq -r .urls[0].upload_time_iso_8601 <<< $pypi_data)
 fi
 
@@ -31,7 +30,7 @@ readonly baseimage_date=$(<baseimage_date)
 
 if [[ $image_date < $baseimage_date ]]; then
 	echo ::set-output name=should-update::true
-elif [[ $USE_PYPI == true && $image_date < $pypi_date ]]; then
+elif [[ $pypi == true && $image_date < $pypi_date ]]; then
 	echo ::set-output name=should-update::true
 else
 	echo ::set-output name=should-update::false
