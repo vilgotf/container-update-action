6b80ee53f9e3bfe80137aae621399f203f272dc0
Vilgot Fredenberg <vilgot@fredenberg.xyz>
2020-12-28 23:08:26 +0100

    Cleanup if true statements, dropping "[[ ]]"

--- script.sh
+++ script.sh
@@ -11,14 +11,14 @@ trap script_fail ERR
 
 source data
 
-[[ $debug == true ]] && echo 'enabling debug!' && set -x
+$debug && echo enabling debug!; set -x
 
 skopeo inspect docker://docker.io/$baseimage | jq -r .Created > baseimage_date &
 skopeo inspect docker://docker.io/$image | jq -r .Created > image_date &
 
 [[ -n $pypi_project ]] && pypi=true || pypi=false
 
-if [[ $pypi == true ]]; then
+if $pypi; then
 	pypi_data=$(curl -fsSL https://pypi.org/pypi/$pypi_project/json)
 	readonly pypi_date=$(jq -r .urls[0].upload_time_iso_8601 <<< $pypi_data)
 fi
@@ -30,7 +30,7 @@ readonly baseimage_date=$(<baseimage_date)
 
 if [[ $image_date < $baseimage_date ]]; then
 	echo ::set-output name=should-update::true
-elif [[ $pypi == true && $image_date < $pypi_date ]]; then
+elif $pypi && [[ $image_date < $pypi_date ]]; then
 	echo ::set-output name=should-update::true
 else
 	echo ::set-output name=should-update::false
